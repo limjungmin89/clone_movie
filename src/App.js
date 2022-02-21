5061d9fbd26304af67a0c95a07346bdeff8aa41a
Jungmin Lim <jungminim@mz.co.kr>
2022-02-21 14:30:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Strart
+          Movie Strart
         </a>
       </header>
     </div>
