0f2b1c9af21d771e9d94dbca31d9f0ae98f8820f
Jungmin Lim <jungminim@mz.co.kr>
2022-03-02 13:23:03 +0900

    모델 추가

---
+++ src/model/interface/user.ts
@@ -0,0 +1,12 @@
+interface user {
+  id: string
+  name: string
+}
+
+interface user {
+  number: number
+}
+
+// 선언적 확장 가능
+
+export default user
---
+++ src/model/type/user.ts
@@ -0,0 +1,12 @@
+type user = {
+  id: string
+  name: string
+}
+
+type extendUser = user & {
+  number: number
+}
+
+// 선언적 확장 불가능
+
+export default user
