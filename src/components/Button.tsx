f013fa580b627afd509b133586dbffd36f6563a5
Jungmin Lim <jungminim@mz.co.kr>
2022-03-02 13:21:19 +0900

    모델 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,16 +7,21 @@ interface propsType {
 
 const Button = (props: propsType) => {
   const [cnt, setCnt] = useState(0)
-  const handleClick = () => {
+  const [keyword, setKeyword] = useState('')
+  const onClick = () => {
     setCnt((prev) => prev + 1)
   }
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value)
+  }
   useEffect(() => {
-    console.log(cnt)
-  }, [cnt])
+    console.log(keyword)
+  }, [keyword])
   return (
     <div>
+      <input type="text" placeholder="Search here..." onChange={onChange} value={keyword}></input>
       <h1>{cnt}</h1>
-      <button onClick={handleClick} className={'btn'}>
+      <button onClick={onClick} className={'btn'}>
         {props.btnText}
       </button>
     </div>
---
+++ src/model/interface/user.ts
@@ -0,0 +1,12 @@
+interface user {
+    id: string,
+    name: string
+}
+
+interface user {
+    phone: number
+}
+
+// 선언적 확장 가능
+
+export default user
