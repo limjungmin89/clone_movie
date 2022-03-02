e9d1557334d8288b3f1317707abe6b833d117870
Jungmin Lim <jungminim@mz.co.kr>
2022-03-02 12:43:08 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
-import Button from './components/Button'
 
 const App = () => {
   return (
     <div>
       <h1>Welcome</h1>
-      <Button btnText="Click Me" />
     </div>
   )
 }
