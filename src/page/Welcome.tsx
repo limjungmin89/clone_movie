e5783b7965a4b881c439c675785a0c7aa106f831
limjungmin89 <limjungmin89@github.com>
2022-03-06 00:15:59 +0900

    ㅅㅅㅅ

--- src/page/Welcome.tsx
+++ src/page/Welcome.tsx
@@ -8,7 +8,7 @@ const Welcome = () => {
 
   return (
     <div>
-      <h1>hello. This is My React Page!!!</h1>
+      <h1>hello. This is My React Page!!!!!!</h1>
       <button onClick={goMovies}>Movies</button>
     </div>
   )
